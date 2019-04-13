56213a65d907287bca05e6cd7eb4c2806216cfe8
mia <mia_marklee@hotmail.com>
2019-04-13 10:57:49 +0800

    Initial Commit

---
+++ 作業8-snow and sakura/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  作業8-snow and sakura
+//
+//  Created by apple on 2019/4/13.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
